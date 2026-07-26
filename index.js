a042b1437734eb3a02158e360abde9f40d8b829f
Souparna Patra <patrasouparno@gmail.com>
2026-07-26 19:18:53 +0530

    day 01

---
+++ index.js
@@ -0,0 +1,174 @@
+console.log("hello,world");
+console.log(32)
+console.log(5>3)
+console.log(5+3)
+console.log("the value of 32 is :",32,"hey"+"oi oi oi")
+console.log(`hare krishna this is
+    gurukulam stw`)
+console.log("5"+"3")
+console.log("5"-"3")
+console.log("5"==5)
+console.log("5"===5)
+console.log(`this is a text, this is a value of 2+3: ${2+3}`
+)
+const name ="gklm stw"
+console.log('hey',name,'how are you')
+let a=5
+let b=3
+console.log(a+b)
+a=10
+console.log(a+b)
+console.log(typeof a)
+console.log(typeof name)
+a="hello"
+//name=123
+const arr=[1,2,3,4,5]
+console.log(arr)
+console.log(arr[1])
+const input =prompt("enter your name")
+console.log("your name is:",input)
+console.log(typeof arr)
+const obj={
+    name:"s patra",
+    age:23,
+    isStudent:true    // here comma may or may not be given in last line of obj unlike json wher comma should be avoided
+}
+console.log(typeof obj==typeof arr)
+const abc=null;
+const xyz=undefined;
+console.log((abc==xyz))
+console.log(abc===xyz)
+console.log(xyz)
+console.log(obj.name)
+console.log(obj.name.first)
+console.log(obj.school)
+//console.log(obj.school.first)
+console.log('5'!=5)
+console.log('5'!==5)
+console.log('5'==5||obj.school.name)
+//console.log('5'==5|obj.school.name)
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    if(true){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    if(null){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    if(0){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    if(-1){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    const str=''
+    if(str){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    const str=''
+    let confusion=500+'abc'
+    console.log(confusion)
+    if(str||confusion){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    const str=''
+    let confusion=500*'abc'
+    console.log(confusion)
+    if(str||confusion){
+        console.log("will it print?")
+    }
+}
+if(2>5){
+    console.log("mathematics is wrong")
+}
+else{
+    console.log("this will print")
+    const str=''
+    let confusion=5/0
+    console.log(confusion)
+    if(str||confusion){
+        console.log("will it print?")
+    }
+}
+/*let marks={
+    math:90,
+    phy:75,
+    chem:85
+}
+let total= marks.math+marks.phy+marks.chem
+console.log(total)
+/let marks={
+    math:90,
+    phy:75,
+    chem:85
+}
+let total= marks.math+marks.phy+marks.chem
+//total+=(marks.practical&& marks.practical.phy)||0
+console.log(total)
+let marks={
+    math:90,
+    phy:75,
+    chem:85
+    practical:{
+        phy:65
+    }
+}
+let total= marks.math+marks.phy+marks.chem
+total+=(marks.practical&& marks.practical.phy)||0
+console.log(total)
+let marks={
+    math:90,
+    phy:75,
+    chem:85
+}
+let total= marks.math+marks.phy+marks.chem
+total+=marks.practical.phy
+console.log(total)*/
+console.log(true==2)
+console.log(5-'a')
+console.log(true+1)
+console.log(5+parseInt('4a'))
+console.log(0.1+0.2)
+console.log((0.1+0.2).toFixed(2))
+const newChar='spatra'
+console.log(newChar=='Spatra')
+console.log(newChar.toLowerCase=='Spatra')
