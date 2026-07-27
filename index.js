c00a9b1cd9da5d5bf446238ca4fa0c6062027461
Souparna Patra <patrasouparno@gmail.com>
2026-07-27 11:28:18 +0530

    creating conflict change in second branch

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ console.log("your name is:",input)
 console.log(typeof arr)
 const obj={
     name:"s patra",
-    age:26,
+    age:85,
     isStudent:true    // here comma may or may not be given in last line of obj unlike json wher comma should be avoided
 }
 console.log(typeof obj==typeof arr)
